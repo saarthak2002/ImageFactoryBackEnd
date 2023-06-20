8a23ff63a0274299ee69e26e64d41b9cbd85175c
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-06-20 15:26:30 +0530

    added backend support for user feed view

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Post = require('../../models/Post.js');
+const UserDetails = require('../../models/UserDetails.js');
 
 router.get('/', (request, result) => {
     Post.find()
@@ -16,6 +17,7 @@ router.get('/:id', (request, result) => {
         .catch(err => result.status(404).json({ nopostfound: 'No post found' }));
 })
 
+// add a post
 router.post('/', (request, result) => {
     Post.create(request.body)
         .then(post => result.json({ msg: 'Post added successfully' }))
@@ -29,5 +31,23 @@ router.get('/user/:id', (request, result) => {
         .then(posts => result.json(posts))
         .catch(err => result.status(404).json({ nopostsfound: 'No posts found' }));
 });
+
+// get feed for logged in user
+router.get('/feed/:user_id', (request, result) => {
+    UserDetails
+        .findOne({ user: request.params.user_id })
+        .then(userDetails => {
+            if(userDetails.following.length > 0) {
+                Post.find({ postedByUser: { $in: userDetails.following } })
+                    .sort({ createdAt: -1 })
+                    .then(posts => result.json(posts))
+                    .catch(err => result.status(404).json({ nopostsfound: 'No posts found' }));
+            }
+            else {
+                result.json([]);
+            }
+        })
+        .catch(err => result.status(404).json({ nouserdetailsfound: 'No user details found' }));
+})
 
 module.exports = router;

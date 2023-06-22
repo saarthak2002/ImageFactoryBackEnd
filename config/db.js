c12f8fdfdf3e34ee887698a7721ea0e1f8617d7b
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-06-23 00:36:53 +0530

    edited deploy config

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const config = require('config');
-// const db = config.get('mongoURI');
-// const db = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   console.log(process.env.MONGODB_URI);
